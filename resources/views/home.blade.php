fafc04f32af40685d2c130bfa6b619418b093f5d
jestin-g <jestingabriel@gmail.com>
2020-04-25 15:45:12 +0200

    rename view "student.profile" to "student.show"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="row justify-content-center">
     <div class="col-md-8">
         @can('manage-students')
-        @include('student.profile')
+            @include('student.show')
         @endcan
     </div>
 </div>
