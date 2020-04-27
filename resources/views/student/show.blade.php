f26c8d856f8d25bcdd566c7880b8f9d751c9ba3b
Gabriel <jest1gabriel@gmail.com>
2020-04-27 09:51:33 +0200

    create button "modifier"

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -1,5 +1,20 @@
 <div class="card">
-    <div class="card-header">Mon profil</div>
+    <div class="card-header">
+        <div class="row">
+            <div class="col">
+                Mon profil
+            </div>
+            @if(auth()->user()->student()->exists())
+            <div class="col">
+                <form action="{{ route('student.students.edit', auth()->user()->student) }}" method="POST">
+                    @csrf
+                    @method('GET')
+                    <button type="submit" class="btn btn-primary float-right">modifier</button>
+                </form>
+            </div>
+            @endif
+        </div>
+    </div>
     <div class="card-body">
         @if(auth()->user()->student()->exists())
 
@@ -8,7 +23,7 @@
             <form action="{{ route('student.students.store')}}" method="POST">
                 @csrf
                 @method('POST')
-                <button type="submit" class="btn btn-primary">Créer mon profil</button>
+                <button type="submit" class="btn btn-success">Créer mon profil étudiant</button>
             </form>
         </td>
         @endif
