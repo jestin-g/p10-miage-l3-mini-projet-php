aa390360374112218eb7feb47086c53f5b5c8183
Gabriel <jest1gabriel@gmail.com>
2020-04-27 11:07:21 +0200

    Student fields at NULL default

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -17,7 +17,34 @@
     </div>
     <div class="card-body">
         @if(auth()->user()->student()->exists())
-
+        <table class="table">
+            <tbody>
+              <tr>
+                <th scope="row">Nom</th>
+                <td>{{ $student->name ?? 'à renseigner'}}</td>
+              </tr>
+              <tr>
+                <th scope="row">Prénom</th>
+                <td>{{ $student->surname ?? 'à renseigner'}}</td>
+              </tr>
+              <tr>
+                <th scope="row">Numéro carte étudiante</th>
+                <td>{{ $student->num_student_card ?? 'à renseigner'}}</td>
+              </tr>
+              <tr>
+                <th scope="row">Date de naissance</th>
+                <td>{{ $student->birth_date ?? 'à renseigner'}}</td>
+              </tr>
+              <tr>
+                <th scope="row">Adresse postale</th>
+                <td>{{ $student->address ?? 'à renseigner'}}</td>
+              </tr>
+              <tr>
+                <th scope="row">Numéro de téléphone</th>
+                <td>{{ $student->phone_number ?? 'à renseigner'}}</td>
+              </tr>
+            </tbody>
+          </table>
         @else
         <td>
             <form action="{{ route('students.store')}}" method="POST">
