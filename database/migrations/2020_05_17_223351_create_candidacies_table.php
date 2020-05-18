3c8abf977b158dad13d878fe59b47b6d24aaf15f
jestin-g <jest1gabriel@gmail.com>
2020-05-18 23:01:48 +0200

    suppression des constraints contraignantes

--- database/migrations/2020_05_17_223351_create_candidacies_table.php
+++ database/migrations/2020_05_17_223351_create_candidacies_table.php
@@ -15,16 +15,9 @@ class CreateCandidaciesTable extends Migration
     {
         Schema::create('candidacies', function (Blueprint $table) {
             $table->id();
-
             $table->bigInteger('student_id')->unsigned()->unique();
-            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
-
-            $table->bigInteger('candidadcy_grade_id')->unsigned()->unique();
-            $table->foreign('candidadcy_grade_id')->references('id')->on('candidacy_grades')->onDelete('cascade');
-
-            $table->bigInteger('candidacy_status_id')->unsigned()->unique();
-            $table->foreign('candidacy_status_id')->references('id')->on('candidacy_statuses')->onDelete('cascade');
-
+            $table->bigInteger('candidadcy_grade_id')->unsigned();
+            $table->bigInteger('candidacy_status_id')->unsigned();
             $table->timestamps();
         });
     }
@@ -36,13 +29,6 @@ class CreateCandidaciesTable extends Migration
      */
     public function down()
     {
-        Schema::table('candidacies', function (Blueprint $table){
-            $table->dropForeign('candidacies_student_id_foreign');
-        });
-
-        Schema::table('candidacies', function (Blueprint $table){
-            $table->dropForeign('candidacies_grade_id_foreign');
-        });
         Schema::dropIfExists('candidacies');
     }
 }
